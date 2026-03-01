08ff10c9839ade8aada1b1a2cf79322c63eaadc3
sumin150130-droid <sumin150130@gmail.com>
2026-03-01 21:51:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-document.getElementById("myButton").addEventListener("click", function() {
-  alert("Á¦´ë·Î ÀÛµ¿ Áß");
+import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged }
+from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+const logoutBtn = document.getElementById("logoutBtn");
+
+const provider = new GoogleAuthProvider();
+
+loginBtn.addEventListener("click", async () => {
+  try {
+    await signInWithPopup(window.auth, provider);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+logoutBtn.addEventListener("click", async () => {
+  await signOut(window.auth);
+});
+
+onAuthStateChanged(window.auth, (user) => {
+  if (user) {
+    console.log("ë¡œê·¸ì¸ë¨:", user.email);
+    loginBtn.style.display = "none";
+    logoutBtn.style.display = "inline-block";
+  } else {
+    loginBtn.style.display = "inline-block";
+    logoutBtn.style.display = "none";
+  }
 });
